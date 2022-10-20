6ba6e67dee6e1f6ca1f310d5f22977e4b7579dbf
Alexander Rudolph <alexr30@web.de>
2022-10-20 20:56:02 +0200

    Different way to build

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/Money/"
+  base: "/Money/",
+  build: {
+    outDir: "./docs"
+  }
 })
