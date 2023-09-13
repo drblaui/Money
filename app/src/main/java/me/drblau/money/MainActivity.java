a02af94f03bc12c52ef9603ece03f45d652189b1
Alexander Rudolph <a.rudolph@itc.drk.de>
2023-09-13 17:20:21 +0200

    Upgrade to ViewPager2

--- app/src/main/java/me/drblau/money/MainActivity.java
+++ app/src/main/java/me/drblau/money/MainActivity.java
@@ -3,9 +3,10 @@ package me.drblau.money;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import me.drblau.money.databinding.ActivityMainBinding;
 import me.drblau.money.ui.main.SectionsPagerAdapter;
@@ -19,10 +20,10 @@ public class MainActivity extends AppCompatActivity {
         me.drblau.money.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), getLifecycle(), this);
+        ViewPager2 viewPager = binding.viewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
-        tabs.setupWithViewPager(viewPager);
+        new TabLayoutMediator(tabs, viewPager, (tab, pos) -> tab.setText(sectionsPagerAdapter.getPageTitle(pos))).attach();
     }
 }
--- app/src/main/java/me/drblau/money/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/me/drblau/money/ui/main/SectionsPagerAdapter.java
@@ -7,7 +7,8 @@ import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import java.util.Locale;
 
@@ -17,32 +18,31 @@ import me.drblau.money.R;
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-public class SectionsPagerAdapter extends FragmentPagerAdapter {
+public class SectionsPagerAdapter extends FragmentStateAdapter {
 
     @StringRes
     private static final int[] TAB_TITLES = new int[]{R.string.month, R.string.year, R.string.all, R.string.settings};
     private final Context mContext;
 
-    public SectionsPagerAdapter(Context context, FragmentManager fm) {
-        super(fm);
+    public SectionsPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle, Context context) {
+        super(fragmentManager, lifecycle);
         mContext = context;
     }
 
+
+    @Nullable
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
     @NonNull
     @Override
-    public Fragment getItem(int position) {
+    public Fragment createFragment(int position) {
         String title = mContext.getString(TAB_TITLES[position]);
         return BaseFragment.newInstance(title.toLowerCase(Locale.ROOT), position);
     }
 
-    @Nullable
     @Override
-    public CharSequence getPageTitle(int position) {
-        return mContext.getResources().getString(TAB_TITLES[position]);
-    }
-
-    @Override
-    public int getCount() {
+    public int getItemCount() {
         return TAB_TITLES.length;
     }
 }
